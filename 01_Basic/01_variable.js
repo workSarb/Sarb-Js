9ac128638b2b13a2d3aa2c8b2e9c2f3ac985fa69
workSarb <104186506+workSarb@users.noreply.github.com>
2024-01-31 07:01:32 +0000

    Variable Investigation\

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,15 @@
+const userId = 1
+let userName = "Sarb"
+var userPass = "123"
+userCity = "Karnal"
+let userPhone
+
+//userId = 8 //you can not change the value of const
+
+userName = "Js Script"
+userPass = "Test@gmail.com"
+userCity = "Jaiput"
+
+console.log(userName) // this print single value
+
+console.table([userId,userName,userPass,userCity,userPhone])
