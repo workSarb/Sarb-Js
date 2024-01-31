22c91cd0d1d3fc69a854813c196f8b8680756646
workSarb <104186506+workSarb@users.noreply.github.com>
2024-01-31 07:49:21 +0000

    Opreations

--- 01_Basic/03_ConversionOperation.js
+++ 01_Basic/03_ConversionOperation.js
@@ -1,9 +1,34 @@
 let userId  = "sarb"
 
-console.log(typeof userId)
+/* ============ Conversion. ====================*/
 
+//console.log(typeof userId)
 let userIdInNumber = Number(userId) // same use for String, Boolean
 
-console.log(typeof userIdInNumber)
-console.log(userIdInNumber)
+//console.log(typeof userIdInNumber)
+//console.log(userIdInNumber)
 
+/* ============ Operations. ====================*/
+
+let age = 32
+let negNumber =-age
+//console.log(negNumber)
+
+//console.log(2+2)
+//console.log(2*2)
+//console.log(2-2)
+//console.log(2**2)
+//console.log(2/2)
+//console.log(2%2)
+
+
+let str1 = "Sarbjyot"
+let str2 = " Singh"
+
+let result = str1 + str2
+console.log(result)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
