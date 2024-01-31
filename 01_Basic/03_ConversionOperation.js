0369c10e76ff8f1beeb51607bdfba841eb22b638
workSarb <104186506+workSarb@users.noreply.github.com>
2024-01-31 07:33:38 +0000

    ConversionOperation

---
+++ 01_Basic/03_ConversionOperation.js
@@ -0,0 +1,9 @@
+let userId  = "sarb"
+
+console.log(typeof userId)
+
+let userIdInNumber = Number(userId) // same use for String, Boolean
+
+console.log(typeof userIdInNumber)
+console.log(userIdInNumber)
+
