7701a0b070c3f9456c14312811f97bbba85814a6
workSarb <104186506+workSarb@users.noreply.github.com>
2024-01-31 07:20:07 +0000

    Learn DataType

---
+++ 01_Basic/02_DataType.js
@@ -0,0 +1,17 @@
+"use strict"; // we define this to confirm we used the morden version of js
+
+let userName = "Sarb" // string value
+let age = 32.  // int value
+let isMarried = true // boolean value
+let state = null // standalone value
+
+
+//synbol => uniqe 
+// bigint => 
+//object
+
+
+// typeof you can use this to check the data type of valid value
+console.log(typeof null) // the type fo null is object
+
+console.table([userName,age,isMarried])
